af37b06f6dc4cc08b5ae7081a79e4a19ca85763d
KingGodX <1237450film@gmail.com>
2025-03-22 04:05:29 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 local PlaceId = tostring(game.PlaceId)
 
-if PlaceId == "87039211657390" and PlaceId == "128336380114944" then -- Arise Crossover
+if PlaceId == "87039211657390" or PlaceId == "128336380114944" then -- Arise Crossover
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e55c789ededce8acc654cb44e464597b.lua"))()
 end
