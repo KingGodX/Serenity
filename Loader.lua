641b7f683b07f5cecf551fa1377bf0f7d05833a5
KingGodX <1237450film@gmail.com>
2025-04-22 05:31:02 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,10 @@
 local PlaceId = tostring(game.PlaceId)
+getgenv().License = script_key or ""
 
-if PlaceId == "104715542330896" then -- Block Spin
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dbb29fb2adcc81e39769867dd80a1640.lua"))()
+if script_key then
+    if PlaceId == "104715542330896" then -- Block Spin
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dbb29fb2adcc81e39769867dd80a1640.lua"))()
+    end 
+else
+    game:GetService("Players").LocalPlayer:Kick("You don't have a key")
 end
