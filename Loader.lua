165a8626ecca745c52bbb0dc915510d2fa005a98
KingGodX <1237450film@gmail.com>
2025-06-02 18:53:41 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ local PlaceId = tostring(game.PlaceId)
 getgenv().License = script_key or ""
 
 if script_key then
-    if PlaceId == "104715542330896" then -- Block Spin
+    if PlaceId == "104715542330896" or PlaceId == "97556409405464" then -- Block Spin
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dbb29fb2adcc81e39769867dd80a1640.lua"))()
     end 
 else
